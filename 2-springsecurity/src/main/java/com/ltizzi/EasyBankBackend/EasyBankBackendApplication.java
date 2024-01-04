49a932cbf14aede32b6de204daa550eda9adeb35
ltizzi <terlizzileonardo@gmail.com>
2024-01-04 15:38:08 -0300

    section 3 started, using only one project so I renamed the project main package to Easy Bank Backend

--- 2-springsecurity/src/main/java/com/ltizzi/EasyBankBackend/EasyBankBackendApplication.java
+++ 2-springsecurity/src/main/java/com/ltizzi/EasyBankBackend/EasyBankBackendApplication.java
@@ -1,8 +1,7 @@
-package com.ltizzi.springsecsection2;
+package com.ltizzi.EasyBankBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class EasyBankBackendApplication {
--- 2-springsecurity/src/main/java/com/ltizzi/EasyBankBackend/config/ProjectSecurityConfig.java
+++ 2-springsecurity/src/main/java/com/ltizzi/EasyBankBackend/config/ProjectSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ltizzi.springsecsection2.config;
+package com.ltizzi.EasyBankBackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
