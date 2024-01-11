79fa9d87dd9ba8cf77766f542047652a357de297
ltizzi <terlizzileonardo@gmail.com>
2024-01-11 18:34:47 -0300

    lesson 8 start

--- 2-springsecurity/src/main/java/com/ltizzi/EasyBankBackend/EasyBankBackendApplication.java
+++ 2-springsecurity/src/main/java/com/ltizzi/EasyBankBackend/EasyBankBackendApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EasyBankBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EasyBankBackendApplication.class, args);
 	}
 
