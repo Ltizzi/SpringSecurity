a3db0bbd101b319b58057c5b1e552102ae2a3d46
ltizzi <terlizzileonardo@gmail.com>
2024-01-11 13:01:37 -0300

    lesson 7 started.

---
+++ 2-springsecurity/src/main/java/com/ltizzi/EasyBankBackend/model/Authority.java
@@ -0,0 +1,10 @@
+package com.ltizzi.EasyBankBackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="authorities")
+public class Authority {
+    
+}
