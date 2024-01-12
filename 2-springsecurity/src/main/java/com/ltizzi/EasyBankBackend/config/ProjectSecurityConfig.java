4ec87b905294e62a1c5c7c6465b9040390649a4b
ltizzi <terlizzileonardo@gmail.com>
2024-01-12 14:59:51 -0300

    lesson 9 start, not 8, sorry

--- 2-springsecurity/src/main/java/com/ltizzi/EasyBankBackend/config/ProjectSecurityConfig.java
+++ 2-springsecurity/src/main/java/com/ltizzi/EasyBankBackend/config/ProjectSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -20,6 +19,7 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 
@@ -31,9 +31,11 @@ public class ProjectSecurityConfig {
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName("_csrf");
 
+        //REMOVIDO LUEGO DE USAR JWT (YA NO SE USA MAS JSESSIONID)
         //se crea un contexto de seguridad que envia la cookie en el primer login (jsessionID)
-        http.securityContext((context)-> context.requireExplicitSave(false))
-                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+        //                http.securityContext((context)-> context.requireExplicitSave(false))
+//                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
@@ -42,6 +44,7 @@ public class ProjectSecurityConfig {
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
                         config.setMaxAge(3600L);
                         return config;
                     }
