2e02767c5725105b2ad898e20665cff551977b28
ltizzi <terlizzileonardo@gmail.com>
2024-01-16 13:06:03 -0300

    added a new spring app for oauth lesson with github

---
+++ testoauth/src/main/java/com/testoauth/testoauth/TestoauthApplication.java
@@ -0,0 +1,13 @@
+package com.testoauth.testoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestoauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestoauthApplication.class, args);
+	}
+
+}
---
+++ testoauth/src/test/java/com/testoauth/testoauth/TestoauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testoauth.testoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestoauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
